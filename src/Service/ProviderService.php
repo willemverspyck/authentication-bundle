0cec70f034e62272ede6aa8b141b78e54bc579e8
Willem Verspyck <willemverspyck@users.noreply.github.com>
2026-01-08 14:38:51 +0100

    Improve autowire

--- src/Service/ProviderService.php
+++ src/Service/ProviderService.php
@@ -4,32 +4,18 @@ declare(strict_types=1);
 
 namespace Spyck\AuthenticationBundle\Service;
 
-use Countable;
-use Exception;
-use IteratorAggregate;
 use Spyck\AuthenticationBundle\Provider\ProviderInterface;
-use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
+use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
+use Symfony\Component\DependencyInjection\ServiceLocator;
 
 readonly class ProviderService
 {
-    /**
-     * @param Countable&IteratorAggregate $providers
-     */
-    public function __construct(#[AutowireIterator(tag: 'spyck.authentication.provider', defaultIndexMethod: 'getName')] private iterable $providers)
+    public function __construct(#[AutowireLocator(services: 'spyck.authentication.provider', defaultIndexMethod: 'getName')] private ServiceLocator $serviceLocator)
     {
     }
 
-    /**
-     * @throws Exception
-     */
     public function getProvider(string $name): ProviderInterface
     {
-        foreach ($this->providers->getIterator() as $index => $provider) {
-            if ($index === $name) {
-                return $provider;
-            }
-        }
-
-        throw new Exception(sprintf('Provider "%s" not found', $name));
+        return $this->serviceLocator->get($name);
     }
 }
